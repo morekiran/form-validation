c176308b2c73b4594241c3b6976422ef72f7daea
kiran more <kiranmore1110@gmail.com>
2023-04-11 21:30:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Form from './component/Form/Form';
+import CustomForm from './component/Form/Form';
 
 function App() {
   return (
     <div>
-      <Form/>
+      <CustomForm/>
     </div>
   );
 }
--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,52 +1,65 @@
-import React,{useState} from 'react'
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from "yup";
+import YupPassword from 'yup-password';
 import "./Form.css"
 
-const Form = () => {
+const CustomForm = () => {
 
-    const [firstName,setFirstName]=useState("");
-    const [lastName,setLastName]=useState("");
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+  YupPassword(yup);
 
-    const submitHandler = (e) =>{
-        e.preventDefault()
-        let data = {
-            firstName,
-            lastName,
-            email,
-            password,
-        }
-        alert("Data received");
+  const formValidationSchema = yup.object().shape({
+    name: yup.string().required("Name Can't Be Blank"),
+    email: yup.string().required("Email Can't Be Blank").email("Please Enter Valid Email"),
+
+    mobNumber: yup.string().length(10).required("MobNumber Can't Be Blank"),
+
+    password: yup.string().min(8).max(16).required("Password Can't Be Blank").minLowercase(1,"Min 1 Lowercase Letter Is Required").minUppercase(1,"Min 1 Uppercase Letter Is Required").minNumbers(1,"Min 1 Digit Is Required").minSymbols(1,"Min 1 Special Character Is Required "),
+
+    confirmpassword: yup.string().required("confirmpassword Can't Be Blank")
+    .oneOf([yup.ref("password")], "Password Must Match"),
+
+  });
+
+  return (
+    <Formik initialValues={{
+      name: "",
+      email: "",
+      mobNumber: "" ,
+      password: "",
+      confirmpassword: "",
+    }}
+
+      validationSchema={formValidationSchema}
+
+      onSubmit={(data) => {
+        //  console.log(data.confirmpassword === data.password)
         console.log(data);
-    }
+      }}
+    >
+      <Form className='container'>
+        <h1>Form Handling</h1>
 
-    return (
-        <div className='container'>
+        <Field type="text" name="name" placeholder="Enter Your Name" />
+        <p className='errorLine'><ErrorMessage name="name" /></p>
 
-            <h1>Form Handling</h1>
+        <Field type="text" name="email" placeholder="Enter Your Email " />
+        <p className='errorLine'><ErrorMessage name="email" /></p>
 
-          <form onSubmit={submitHandler}>
-            <input type="text"placeholder="Enter Your First Name" 
-             value={firstName}
-             onChange={(e)=>setFirstName(e.target.value)}required/>
+        <Field type="password" name="password" placeholder="Enter Your Password" />
+        <p className='errorLine'><ErrorMessage name="password" /></p>
 
-            <input type="text"placeholder="Enter Your Last Name"
-             value={lastName}
-             onChange={(e)=>setLastName(e.target.value)}required/> 
+        <Field type="password" name="confirmpassword" placeholder="Enter Confirm Password" />
+        <p className='errorLine'><ErrorMessage name="confirmpassword" /></p>
 
-            <input type="text"placeholder="Enter Your Email " 
-            value={email}
-            onChange={(e)=>setEmail(e.target.value)}required/>
+        <Field type="number" name="mobNumber" placeholder="Enter Your mobNumber" />
+        <p className='errorLine'><ErrorMessage name="mobNumber" /></p>
 
-            <input type="password"placeholder="Enter Your Password" 
-            value={password}
-            onChange={(e)=>setPassword(e.target.value)} required/>
+        <Field type="submit" value="Submit" className='butn' />
+      </Form>
 
-            <input type="submit" className='butn' />
-          </form>
-       
-        </div>
-    )
+    </Formik>
+  )
 }
 
-export default Form;
+export default CustomForm;
